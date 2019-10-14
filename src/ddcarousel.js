0e7d647404e78128f293841aacce34e8e232f23c
danaildinev <danail_dd@windowslive.com>
2019-10-14 13:18:44 +0300

    fixed babel, reduced dist filesize, foreach polyfill

--- src/ddcarousel.js
+++ src/ddcarousel.js
@@ -657,51 +657,13 @@ class DDCarousel {
 	}
 }
 
+//polyfills
 Number.isInteger =
 	Number.isInteger ||
 	function (value) {
 		return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
 	};
 
-if (!("classList" in document.documentElement) && Object.defineProperty && typeof HTMLElement !== "undefined") {
-	Object.defineProperty(HTMLElement.prototype, "classList", {
-		get: function () {
-			var self = this;
-			function update(fn) {
-				return function (value) {
-					var classes = self.className.split(/\s+/),
-						index = classes.indexOf(value);
-
-					fn(classes, index, value);
-					self.className = classes.join(" ");
-				};
-			}
-
-			var ret = {
-				add: update(function (classes, index, value) {
-					~index || classes.push(value);
-				}),
-
-				remove: update(function (classes, index) {
-					~index && classes.splice(index, 1);
-				}),
-
-				contains: function (value) {
-					return !!~self.className.split(/\s+/).indexOf(value);
-				},
-
-				item: function (i) {
-					return self.className.split(/\s+/)[i] || null;
-				}
-			};
-
-			Object.defineProperty(ret, "length", {
-				get: function () {
-					return self.className.split(/\s+/).length;
-				}
-			});
-
-			return ret;
-		}
-	});
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = Array.prototype.forEach;
 }
