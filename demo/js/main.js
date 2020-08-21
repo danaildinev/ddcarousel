81a6c511d966789b837e219d9f4e2d6dc6ffb4c4
Danail Dinev <9660431+danaildinev@users.noreply.github.com>
2020-08-21 11:00:48 +0300

    demos refactoring and some design changes

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,44 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-	var navContents = document.querySelector("nav.contents");
-	document.getElementById("toggle-nav").addEventListener("click", function (e) {
-		e.preventDefault();
-		if (navContents.classList.contains("show")) {
-			navContents.classList.remove("show");
-			document.body.classList.remove("nav-opened");
-		} else {
-			navContents.classList.add("show");
-			document.body.classList.add("nav-opened");
-		}
-	});
-
-	var defaultCarousel = new DDCarousel({
+var config = {
+	default: {
 		container: ".default",
-	});
-
-	var items = new DDCarousel({
+	},
+	items: {
 		container: ".items",
 		items: 3,
 		nav: true,
 		dots: false,
 		mouseDrag: true
-	});
-
-	var centered = new DDCarousel({
+	},
+	centered: {
 		container: ".centered",
 		items: 3,
 		centerSlide: true,
 		mouseDrag: true
-	});
-
-	var autoHeight = new DDCarousel({
+	},
+	autoHeight: {
 		container: ".autoHeight",
 		items: 3,
 		itemPerPage: true,
 		mouseDrag: true
-	});
-
-	var responsive = new DDCarousel({
+	},
+	responsive: {
 		container: ".responsive",
 		items: 3,
 		nav: true,
@@ -55,9 +38,8 @@ document.addEventListener("DOMContentLoaded", function () {
 				fullWidth: true
 			},
 		}
-	});
-
-	var vertical = new DDCarousel({
+	},
+	vertical: {
 		container: ".vertical",
 		items: 2,
 		itemPerPage: true,
@@ -65,60 +47,45 @@ document.addEventListener("DOMContentLoaded", function () {
 		startPage: 2,
 		mouseDrag: true,
 		autoHeight: false
-	});
-
-	var url = new DDCarousel({
+	},
+	url: {
 		container: ".url",
 		dots: false,
 		urlNav: true,
 		mouseDrag: true
-	});
-
-	var autoplay = new DDCarousel({
+	},
+	autoplay: {
 		container: ".autoplay",
 		autoplay: true,
 		autoplaySpeed: 3000,
 		autoplayPauseHover: true
-	});
-
-	document.getElementById("apStop").addEventListener("click", function () {
-		autoplay.autoplayStop();
-	});
-
-	document.getElementById("apStart").addEventListener("click", function () {
-		autoplay.autoplayStart();
-	});
-
-	var enabledmouse = new DDCarousel({
+	},
+	disabledTouch: {
 		container: ".disabledTouch",
 		touchDrag: false
-	});
-
-	var enabledMouse = new DDCarousel({
+	},
+	enabledMouse: {
 		container: ".enabledMouse",
 		mouseDrag: true
-	});
-
-	var customLabels = new DDCarousel({
+	},
+	customLabels: {
 		container: ".customLabels",
 		dots: false,
 		nav: true,
 		labelNavPrev: "&#x2190;",
 		labelNavNext: "&#x2192;"
-	});
-
-	var customAnimConfig = new DDCarousel({
-		container: ".customAnimConfig",
+	},
+	customAnim: {
+		container: ".customAnim",
 		items: 3,
 		mouseDrag: true,
 		touchSwipeThreshold: 1,
 		touchMaxSlideDist: 1000,
 		swipeSmooth: 0.2,
 		slideChangeDuration: 1.2
-	});
-
-	var eventsExample = new DDCarousel({
-		container: ".eventsExample",
+	},
+	events: {
+		container: ".events",
 		items: 3,
 		mouseDrag: true,
 		touchSwipeThreshold: 1,
@@ -149,34 +116,107 @@ document.addEventListener("DOMContentLoaded", function () {
 		onResized: function (e) {
 			setActiveEvent(e.event);
 		}
+	}
+};
+
+function setActiveEvent(name) {
+	document.getElementById("eventLog").innerHTML += name + "\r\n";
+	document.getElementById("eventLog").scrollTop = document.getElementById("eventLog").scrollHeight;
+
+	document.querySelector(".events-list [data-event='" + name + "']").className = "active";
+	document.querySelector(".events-list [data-event='" + name + "'] .status").innerHTML = "on";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+	var navContents = document.querySelector("nav.contents"),
+		status = document.querySelectorAll(".events-list .status"),
+		eventClass = document.querySelectorAll(".events-list li"),
+		srcTextareas = document.querySelectorAll("textarea[data-slider]"),
+		carouselItem = document.createElement("div"),
+		carousels = document.querySelectorAll(".ddcarousel"),
+		item, src, i, j;
+
+	//menu
+	function navClose() {
+		navContents.classList.remove("show");
+		document.body.classList.remove("nav-opened");
+	}
+	document.getElementById("toggle-nav").addEventListener("click", function (e) {
+		e.preventDefault();
+		if (navContents.classList.contains("show")) {
+			navClose();
+		} else {
+			navContents.classList.add("show");
+			document.body.classList.add("nav-opened");
+		}
+	});
+	window.addEventListener("hashchange", function () {
+		navClose();
 	});
 
-	document.getElementById("resizeContainer").addEventListener("click", function () {
-		document.getElementsByClassName("eventsExample")[0].style.width = Math.floor(Math.random() * 80) + 50 + "%";
-		eventsExample.refresh();
-	});
-
-	var status = document.querySelectorAll(".events-list .status");
-	var eventClass = document.querySelectorAll(".events-list li");
-	for (var index = 0; index < status.length; index++) {
-		status[index].innerHTML = "off";
+	//populate carousels
+	carouselItem.classList.add("box");
+	for (i = 0; i < carousels.length; i++) {
+		for (j = 1; j < 11; j++) {
+			item = carouselItem.cloneNode(true);
+			item.textContent = j;
+			carousels[i].appendChild(item);
+		}
 	}
 
+	//init carousels
+	var defaultCarousel = DDCarousel(config['default']),
+		items = DDCarousel(config['items']),
+		centered = DDCarousel(config['centered']),
+		autoHeight = DDCarousel(config['autoHeight']),
+		responsive = DDCarousel(config['responsive']),
+		vertical = DDCarousel(config['vertical']),
+		url = DDCarousel(config['url']),
+		autoplay = DDCarousel(config['autoplay']),
+		enabledmouse = DDCarousel(config['enabledMouse']),
+		disabledTouch = DDCarousel(config['disabledTouch']),
+		customLabels = DDCarousel(config['customLabels']),
+		customAnim = DDCarousel(config['customAnim']),
+		events = DDCarousel(config['events']);
+
+	//autoplay controls
+	document.getElementById("apStop").addEventListener("click", function () {
+		autoplay.autoplayStop();
+	});
+	document.getElementById("apStart").addEventListener("click", function () {
+		autoplay.autoplayStart();
+	});
+
+	//resize button
+	document.getElementById("resizeContainer").addEventListener("click", function () {
+		document.querySelector(".ddcarousel.events").style.width = Math.floor(Math.random() * 80) + 50 + "%";
+		events.refresh();
+	});
+
+	//events
+	for (i = 0; i < status.length; i++) {
+		status[i].innerHTML = "off";
+	}
 	setInterval(function () {
-		for (var index = 0; index < status.length; index++) {
-			status[index].innerHTML = "off";
+		for (i = 0; i < status.length; i++) {
+			status[i].innerHTML = "off";
 		}
-		for (var i = 0; i < eventClass.length; i++) {
-			eventClass[i].className = "";
+		for (j = 0; j < eventClass.length; j++) {
+			eventClass[j].className = "";
 		}
 	}, 1000);
 
-	function setActiveEvent(name) {
-		document.getElementById("eventLog").innerHTML += name + "\r\n";
-		document.getElementById("eventLog").scrollTop = document.getElementById("eventLog").scrollHeight;
+	//populate src textboxes
+	for (i = 0; i < srcTextareas.length; i++) {
+		var el = srcTextareas[i],
+			source = { ...config[el.getAttribute('data-slider')] };
 
-		document.querySelector(".events-list [data-event='" + name + "']").className = "active";
-		document.querySelector(".events-list [data-event='" + name + "'] .status").innerHTML = "on";
+		source['container'] = undefined;
+		if (source !== undefined) {
+			src = JSON.stringify(source, null, "  ");
+			el.innerHTML = src.replace(/"([^"]+)":/g, '$1:');
+		}
 	}
 });
 
