8755db32a861c263e7f8ad9dab10b4d2820eaaa3
Danail Dinev <9660431+danaildinev@users.noreply.github.com>
2026-03-12 01:57:27 +0200

    added new esm prod build, improved webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 import path from "path";
 import { fileURLToPath } from 'url';
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import TerserPlugin from "terser-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-const baseConfig = () => ({
+const createBaseConfig = (sassStyle = "compressed") => ({
     entry: "./src/ddcarousel.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
     module: {
         rules: [
             {
@@ -24,43 +30,27 @@ const baseConfig = () => ({
                 use: [
                     MiniCssExtractPlugin.loader,
                     "css-loader",
-                    "sass-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sassOptions: {
+                                style: sassStyle,
+                            },
+                        },
+                    },
                 ],
             }]
     },
     resolve: {
-        extensions: [".ts", ".js",]
-    }
+        extensions: [".ts", ".js"]
+    },
 });
 
-function umdProdConfig() {
-    return {
-        ...baseConfig(),
-        mode: "production",
-        output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "ddcarousel.umd.min.js",
-            library: {
-                name: "ddcarousel",
-                type: "umd",
-                export: 'default'
-            },
-            globalObject: "this"
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: "ddcarousel.min.css"
-            }),
-        ],
-        optimization: {
-            minimize: true,
-        },
-    }
-}
-
+// ESM dev build (used for internal testing)
 function esmDevConfig() {
+    // esm build - dev
     return {
-        ...baseConfig(),
+        ...createBaseConfig("expanded"),
         mode: "development",
         devtool: "source-map",
         watch: true,
@@ -95,9 +85,74 @@ function esmDevConfig() {
     };
 }
 
+// ESM production build (for modern browsers - to use by vite, webpack or any modern app)
+function esmProdConfig() {
+    return {
+        ...createBaseConfig("expanded"),
+        mode: "production",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "ddcarousel.esm.js",
+            library: {
+                type: "module"
+            }
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "ddcarousel.css"
+            }),
+        ],
+        optimization: {
+            minimize: true, // removes user comments
+        },
+        experiments: {
+            outputModule: true
+        },
+    }
+};
+
+// UMD build (for browsers <script>, minified)
+function umdConfig() {
+    return {
+        ...createBaseConfig(),
+        mode: "production",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "ddcarousel.umd.min.js",
+            library: {
+                name: "ddcarousel",
+                type: "umd",
+                export: 'default'
+            },
+            globalObject: "this"
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "ddcarousel.min.css"
+            }),
+        ],
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: false,
+                    terserOptions: {
+                        format: {
+                            comments: /@license|^!/ // preserve only license banner
+                        }
+                    }
+                })
+            ]
+        },
+    }
+}
+
 export default (env = {}) => {
     if (env?.dev)
         return esmDevConfig();
 
-    return umdProdConfig();
+    return [
+        esmProdConfig(),
+        umdConfig(),
+    ];
 };
