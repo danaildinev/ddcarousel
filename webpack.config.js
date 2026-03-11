aede777f1ffc1e0940d4db576be56dec53da4b9a
Danail Dinev <9660431+danaildinev@users.noreply.github.com>
2026-03-12 01:17:59 +0200

    added dev server to webpack dev build; new npm scripts for dev and prod build

--- webpack.config.js
+++ webpack.config.js
@@ -5,39 +5,65 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-export default () => {
-    return [{
-        entry: "./src/ddcarousel.ts",
-        output: {
-            path: path.resolve(__dirname, "testing"),
-            clean: true
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    use: {
-                        loader: 'ts-loader',
-                        options: {
-                            transpileOnly: true
-                        }
-                    },
-                    exclude: /node_modules/,
+const baseConfig = () => ({
+    entry: "./src/ddcarousel.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true
+                    }
                 },
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        "sass-loader",
-                    ],
-                }]
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            }]
+    },
+    resolve: {
+        extensions: [".ts", ".js",]
+    }
+});
+
+function umdProdConfig() {
+    return {
+        ...baseConfig(),
+        mode: "production",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "ddcarousel.umd.min.js",
+            library: {
+                name: "ddcarousel",
+                type: "umd",
+                export: 'default'
+            },
+            globalObject: "this"
         },
-        resolve: {
-            extensions: [".ts", ".tsx", ".js", ".mjs"]
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "ddcarousel.min.css"
+            }),
+        ],
+        optimization: {
+            minimize: true,
         },
+    }
+}
+
+function esmDevConfig() {
+    return {
+        ...baseConfig(),
         mode: "development",
         devtool: "source-map",
+        watch: true,
         output: {
             path: path.resolve(__dirname, "testing"),
             filename: "ddcarousel.esm.js",
@@ -50,11 +76,28 @@ export default () => {
                 filename: "ddcarousel.css"
             })
         ],
+        devServer: {
+            /*devMiddleware: {
+                writeToDisk: true
+            },*/
+            static: {
+                directory: path.join(__dirname, "testing"),
+            },
+            compress: true,
+            port: 9000,
+        },
         experiments: {
             outputModule: true
         },
         optimization: {
             minimize: false
         }
-    }]
+    };
+}
+
+export default (env = {}) => {
+    if (env?.dev)
+        return esmDevConfig();
+
+    return umdProdConfig();
 };
