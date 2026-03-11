e85eb33be566743686a13a0ea7ff958f190adf8d
Danail Dinev <9660431+danaildinev@users.noreply.github.com>
2026-03-11 23:38:25 +0200

    implemented config in carousel, sample init

--- src/core/carousel.ts
+++ src/core/carousel.ts
@@ -1,4 +1,31 @@
+import type { CarouselConfig } from "../types/carousel.config";
+import { Config } from "./config";
+
 export default class Carousel {
-    constructor(config: {}) {
+    #config!: Config;
+    #initialized: boolean = false;
+
+    constructor(config: CarouselConfig) {
+        if (this.#initialized)
+            throw Error("Already initialized!");
+
+        if (config !== undefined)
+            this.init(config);
+    }
+
+    init(config: CarouselConfig) {
+        if (this.#initialized) {
+            console.warn("Already initialized!");
+            return;
+        }
+
+        this.#config = new Config(config);
+
+        this.#initialized = true;
+    }
+
+    destroy(fullReset: boolean) {
+        this.#config.reset();
+        this.#config = null!;
     }
 }
--- src/ddcarousel.ts
+++ src/ddcarousel.ts
@@ -1,6 +1,7 @@
 import "./ddcarousel.scss";
 import Carousel from "./core/carousel";
+import type { CarouselConfig } from "./types/carousel.config";
 
-export default function ddcarousel(config: {}) {
+export default function ddcarousel(config: CarouselConfig) {
     return new Carousel(config);
 }
