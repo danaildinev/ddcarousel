74822c19b57a0b3ea06c637c7f51cd09368648e7
Danail Dinev <9660431+danaildinev@users.noreply.github.com>
2026-03-21 17:24:36 +0200

    demos updated and cleared legacy code and added the new stuff

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,162 +1,62 @@
-var config = {
-	default: {
-		container: ".default",
-	},
-	items: {
-		container: ".items",
-		items: 3,
-		nav: true,
-		dots: false,
-	},
-	centered: {
-		container: ".centered",
-		items: 3,
-		centerSlide: true,
-	},
-	autoHeight: {
-		container: ".autoHeight",
-		items: 3,
-		itemPerPage: true,
-	},
-	responsive: {
-		container: ".responsive",
-		items: 3,
-		nav: true,
-		dots: false,
-		responsive: {
-			480: {
-				items: 1,
-				nav: false,
-				dots: true,
-			},
-			768: {
-				items: 2,
-				fullWidth: true
-			},
-		}
-	},
-	vertical: {
-		container: ".vertical",
-		items: 2,
-		itemPerPage: true,
-		vertical: true,
-		startPage: 2,
-		autoHeight: false
-	},
-	url: {
-		container: ".url",
-		dots: false,
-		urlNav: true,
-	},
-	lazy: {
-		container: ".lazy",
-		autoHeight: false,
-		items: 2,
-		lazyLoad: true,
-		lazyPreload: true,
-		lazyPreloadSlides: 2,
-	},
-	autoplay: {
-		container: ".autoplay",
-		autoplay: true,
-		autoplaySpeed: 3000,
-		autoplayPauseHover: true
-	},
-	disabledTouch: {
-		container: ".disabledTouch",
-		touchDrag: false
-	},
-	disabledMouse: {
-		container: ".disabledMouse",
-		mouseDrag: false
-	},
-	customLabels: {
-		container: ".customLabels",
-		dots: false,
-		nav: true,
-		labelNavPrev: "< Prev",
-		labelNavNext: "Next >"
-	},
-	customAnim: {
-		container: ".customAnim",
-		items: 3,
-		touchSwipeThreshold: 1,
-		touchMaxSlideDist: 1000,
-		swipeSmooth: 0.2,
-		slideChangeDuration: 1.2
-	},
-	keyboardNav: {
-		container: ".keyboardNav",
-		items: 3,
-		keyboardNavigation: true
-	},
-	events: {
-		container: ".events",
-		items: 2,
-		touchSwipeThreshold: 1,
-		swipeSmooth: 0.2,
-		slideChangeDuration: 1.2,
-		callbacks: true,
-		onInitialize: function (e) {
-			setActiveEvent(e.event);
-		},
-		onInitialized: function (e) {
-			setActiveEvent(e.event);
-		},
-		onDrag: function (e) {
-			setActiveEvent(e.event);
-		},
-		onDragging: function (e) {
-			setActiveEvent(e.event);
-		},
-		onDragged: function (e) {
-			setActiveEvent(e.event);
-		},
-		onChanged: function (e) {
-			setActiveEvent(e.event);
-		},
-		onTransitionend: function (e) {
-			setActiveEvent(e.event);
-		},
-		onResized: function (e) {
-			setActiveEvent(e.event);
-		},
-		onDestroy: function (e) {
-			setActiveEvent(e.event);
-		},
-		onDestroyed: function (e) {
-			setActiveEvent(e.event);
+document.addEventListener("DOMContentLoaded", () => {
+	const log = document.getElementById("eventLog"),
+		navContents = document.querySelector("nav.contents"),
+		eventItem = document.querySelectorAll(".events-list li");
+
+	// set initial value to event labels
+	function resetEventLabels() {
+		for (i = 0; i < eventItem.length; i++) {
+			eventItem[i].className = "";
+			eventItem[i].querySelector(".status").innerHTML = "off";
 		}
 	}
-};
+	resetEventLabels();
+	setInterval(() => resetEventLabels(), 1000);
 
-function setActiveEvent(name) {
-	writeLog(name);
-	document.querySelector(".events-list [data-event='" + name + "']").className = "active";
-	document.querySelector(".events-list [data-event='" + name + "'] .status").innerHTML = "on";
-}
+	// populate carousels with placeholder items inside
+	const carousels = document.querySelectorAll(".ddcarousel"),
+		carouselItem = document.createElement("div");
+	carouselItem.classList.add("box");
+	for (let i = 0; i < carousels.length; i++) {
+		for (let j = 1; j < 11; j++) {
+			const item = carouselItem.cloneNode(true);
+			item.textContent = j;
+			carousels[i].appendChild(item);
+		}
+	}
 
-function writeLog(data) {
-	document.getElementById("eventLog").innerHTML += data + "\r\n";
-	document.getElementById("eventLog").scrollTop = document.getElementById("eventLog").scrollHeight;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-
-	var navContents = document.querySelector("nav.contents"),
-		status = document.querySelectorAll(".events-list .status"),
-		eventClass = document.querySelectorAll(".events-list li"),
-		srcTextareas = document.querySelectorAll("textarea[data-slider]"),
-		carouselItem = document.createElement("div"),
-		carousels = document.querySelectorAll(".ddcarousel"),
-		item, src, i, j;
-
-	//menu
 	function navClose() {
 		navContents.classList.remove("show");
 		document.body.classList.remove("nav-opened");
 	}
-	document.getElementById("toggle-nav").addEventListener("click", function (e) {
+
+	function writeLog(data) {
+		log.innerHTML += data + "\r\n";
+		log.scrollTop = log.scrollHeight;
+	}
+
+	function setActiveEvent(name) {
+		writeLog(name);
+		const event = document.querySelector(`.events-list [data-event='${name}']`);
+		event.className = "active";
+		event.querySelector(".status").innerHTML = "on";
+	}
+
+	function addSlide() {
+		var slide = document.createElement("div");
+		slide.classList.add("box");
+		slide.textContent = "New slide " + Math.floor(Math.random() * 20);
+		document.querySelector('.ddcarousel.events').appendChild(slide);
+		writeLog("Added new slide. You can now initialize the carousel.")
+	}
+
+	function getStatus() {
+		const status = events.getStatus();
+		console.log(status);
+		writeLog(`${getStatus()}\r\n${JSON.stringify(status, null, "  ")}\r\nEnd of getStatus()! Open your console and then you trigger again for more details.`);
+	}
+
+	function toggleNav() {
 		e.preventDefault();
 		if (navContents.classList.contains("show")) {
 			navClose();
@@ -164,200 +64,181 @@ document.addEventListener("DOMContentLoaded", function () {
 			navContents.classList.add("show");
 			document.body.classList.add("nav-opened");
 		}
-	});
-	window.addEventListener("hashchange", function () {
-		navClose();
-	});
-
-	//populate carousels
-	carouselItem.classList.add("box");
-	for (i = 0; i < carousels.length; i++) {
-		for (j = 1; j < 11; j++) {
-			item = carouselItem.cloneNode(true);
-			item.textContent = j;
-			carousels[i].appendChild(item);
-		}
 	}
 
-	//init carousels
-	var defaultCarousel = ddcarousel(config['default']),
-		items = ddcarousel(config['items']),
-		centered = ddcarousel(config['centered']),
-		autoHeight = ddcarousel(config['autoHeight']),
-		responsive = ddcarousel(config['responsive']),
-		vertical = ddcarousel(config['vertical']),
-		url = ddcarousel(config['url']),
-		autoplay = ddcarousel(config['autoplay']),
-		disabledMouse = ddcarousel(config['disabledMouse']),
-		disabledTouch = ddcarousel(config['disabledTouch']),
-		customLabels = ddcarousel(config['customLabels']),
-		customAnim = ddcarousel(config['customAnim']),
-		events = ddcarousel(config['events']),
-		lazy = ddcarousel(config['lazy']),
-		keyboardNav = ddcarousel(config['keyboardNav']);
+	document.getElementById("toggle-nav").addEventListener("click", () => toggleNav());
+	window.addEventListener("hashchange", () => navClose());
 
-	//autoplay controls
-	document.getElementById("apStop").addEventListener("click", function () {
-		autoplay.autoplayStop();
-	});
-	document.getElementById("apStart").addEventListener("click", function () {
-		autoplay.autoplayStart();
-	});
-
-	//resize button
-	document.getElementById("resizeContainer").addEventListener("click", function () {
-		document.querySelector(".ddcarousel.events").style.width = Math.floor(Math.random() * 80) + 50 + "%";
-		writeLog("Carousel was resized. You can now refresh it.")
-
-	});
-	document.getElementById("eventsRefresh").addEventListener("click", function () {
-		events.refresh();
-	});
-
-	//events
-	for (i = 0; i < status.length; i++) {
-		status[i].innerHTML = "off";
-	}
-	setInterval(function () {
-		for (i = 0; i < status.length; i++) {
-			status[i].innerHTML = "off";
-		}
-		for (j = 0; j < eventClass.length; j++) {
-			eventClass[j].className = "";
-		}
-	}, 1000);
-
-	//methods
-	document.getElementById("eventsGoToSlide").addEventListener("click", function () {
-		events.changePage(parseInt(document.getElementById("inputGoToSlide").value));
-	});
-	document.getElementById("eventsPrevSlide").addEventListener("click", function () {
-		events.prevPage();
-	});
-	document.getElementById("eventsNextSlide").addEventListener("click", function () {
-		events.nextPage();
-	});
-	document.getElementById("eventsGetStatus").addEventListener("click", function () {
-		const status = events.getStatus();
-		console.log(status);
-		writeLog("getStatus()");
-		writeLog(JSON.stringify(status, null, "  "));
-		writeLog("End of getStatus()... Open your console and then you trigger again for more details.");
-	});
-	document.getElementById("eventsGetCurrentPage").addEventListener("click", function () {
-		writeLog(events.getCurrentPage())
-	});
-	document.getElementById("eventsGetTotalPages").addEventListener("click", function () {
-		writeLog(events.getTotalPages())
-	});
-	document.getElementById("eventsGetTotalSlides").addEventListener("click", function () {
-		writeLog(events.getTotalSlides())
-	});
-	document.getElementById("eventsDestroyKeep").addEventListener("click", function () {
-		events.destroy();
-	});
-	document.getElementById("eventsDestroy").addEventListener("click", function () {
-		events.destroy(true);
-	});
-	document.getElementById("eventsInit").addEventListener("click", function () {
-		events.init(config['events']);
-	});
-	document.getElementById("eventsAddSlide").addEventListener("click", function () {
-		var slide = document.createElement("div");
-		slide.classList.add("box");
-		slide.textContent = "New slide " + Math.floor(Math.random() * 20);
-		document.querySelector('.ddcarousel.events').appendChild(slide);
-		writeLog("Added new slide. You can now initialize the carousel.")
-	});
-
-	//populate src textboxes
-	for (i = 0; i < srcTextareas.length; i++) {
-		var el = srcTextareas[i],
-			source = {};
-
-		Object.assign(source, config[el.getAttribute('data-slider')]);
-
-		source['container'] = undefined;
-		if (source !== undefined) {
-			src = JSON.stringify(source, null, "  ");
-			el.innerHTML = src.replace(/"([^"]+)":/g, '$1:');
-		}
-	}
-});
-
-//ie
-if (!("classList" in document.documentElement) && Object.defineProperty && typeof HTMLElement !== "undefined") {
-	Object.defineProperty(HTMLElement.prototype, "classList", {
-		get: function () {
-			var self = this;
-			function update(fn) {
-				return function (value) {
-					var classes = self.className.split(/\s+/),
-						index = classes.indexOf(value);
-
-					fn(classes, index, value);
-					self.className = classes.join(" ");
-				};
-			}
-
-			var ret = {
-				add: update(function (classes, index, value) {
-					~index || classes.push(value);
-				}),
-
-				remove: update(function (classes, index) {
-					~index && classes.splice(index, 1);
-				}),
-
-				contains: function (value) {
-					return !!~self.className.split(/\s+/).indexOf(value);
+	const carouselConfigs = {
+		default: {
+			container: ".default",
+		},
+		items: {
+			container: ".items",
+			items: 3,
+			nav: true,
+			dots: false,
+		},
+		centered: {
+			container: ".centered",
+			items: 3,
+			centerSlide: true,
+		},
+		autoHeight: {
+			items: 3,
+			container: ".autoHeight",
+			itemPerPage: true,
+		},
+		responsive: {
+			container: ".responsive",
+			nav: true,
+			dots: false,
+			items: 3,
+			autoplay: false,
+			responsive: {
+				768: {
+					items: 2,
+					dots: true,
+					nav: false,
+					autoplay: true
 				},
-
-				item: function (i) {
-					return self.className.split(/\s+/)[i] || null;
-				}
-			};
-
-			Object.defineProperty(ret, "length", {
-				get: function () {
-					return self.className.split(/\s+/).length;
-				}
-			});
-
-			return ret;
-		}
-	});
-}
-
-if (!Object.assign) {
-	Object.defineProperty(Object, 'assign', {
-		enumerable: false,
-		configurable: true,
-		writable: true,
-		value: function (target) {
-			'use strict';
-			if (target === undefined || target === null) {
-				throw new TypeError('Cannot convert first argument to object');
+				480: {
+					items: 1,
+					dots: true,
+					nav: false,
+					autoplay: true
+				},
 			}
-
-			var to = Object(target);
-			for (var i = 1; i < arguments.length; i++) {
-				var nextSource = arguments[i];
-				if (nextSource === undefined || nextSource === null) {
-					continue;
+		},
+		vertical: {
+			container: ".vertical",
+			items: 2,
+			itemPerPage: true,
+			vertical: true,
+			startPage: 2,
+			autoHeight: false
+		},
+		url: {
+			container: ".url",
+			dots: false,
+			urlNav: true,
+		},
+		lazy: {
+			container: ".lazy",
+			autoHeight: false,
+			items: 2,
+			lazyLoad: true,
+			lazyPreload: true,
+			lazyPreloadSlides: 2,
+		},
+		autoplay: {
+			items: 3,
+			container: ".autoplay",
+			autoplay: true,
+			autoplaySpeed: 3000,
+			autoplayPauseHover: true
+		},
+		disabledTouch: {
+			container: ".disabledTouch",
+			touchDrag: false
+		},
+		disabledMouse: {
+			container: ".disabledMouse",
+			mouseDrag: false
+		},
+		customLabels: {
+			container: ".customLabels",
+			dots: false,
+			nav: true,
+			labelNavPrev: "< Prev",
+			labelNavNext: "Next >"
+		},
+		customAnim: {
+			container: ".customAnim",
+			items: 3,
+			touchSwipeThreshold: 1,
+			touchMaxSlideDist: 1000,
+			swipeSmooth: 0.2,
+			slideChangeDuration: 1.2
+		},
+		keyboardNav: {
+			container: ".keyboardNav",
+			keyboardNavigation: true
+		},
+		events: {
+			container: ".events",
+			items: 3,
+			startPage: 1,
+			autoplay: true,
+			responsive: {
+				700: {
+					items: 2
+				},
+				500: {
+					items: 1
 				}
-				nextSource = Object(nextSource);
-
-				var keysArray = Object.keys(Object(nextSource));
-				for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
-					var nextKey = keysArray[nextIndex];
-					var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-					if (desc !== undefined && desc.enumerable) {
-						to[nextKey] = nextSource[nextKey];
-					}
-				}
-			}
-			return to;
+			},
+			"on:carousel:initalize": e => setActiveEvent("carousel:initalize", e),
+			"on:carousel:initalized": e => setActiveEvent("carousel:initalized", e),
+			"on:module:created": e => setActiveEvent("module:created", e),
+			"on:module:initialized": e => setActiveEvent("module:initialized", e),
+			"on:module:destroyed": e => setActiveEvent("module:destroyed", e),
+			"on:module:autoplay:started": e => setActiveEvent("module:autoplay:started", e),
+			"on:module:autoplay:stopped": e => setActiveEvent("module:autoplay:stopped", e),
+			"on:stage:created": e => setActiveEvent("stage:created", e),
+			"on:stage:resized": e => setActiveEvent("stage:resized", e),
+			"on:page:change": e => setActiveEvent("page:change", e),
+			"on:page:change:request": e => setActiveEvent("page:change:request", e),
+			"on:transition:end": e => setActiveEvent("transition:end", e),
+			"on:drag:start": e => setActiveEvent("drag:start", e),
+			"on:drag:dragging": e => setActiveEvent("drag:dragging", e),
+			"on:drag:end": e => setActiveEvent("drag:end", e),
+			"on:config:changed": e => setActiveEvent("config:changed", e),
 		}
-	});
-}
+	};
+
+	// populate src textboxes
+	const textareas = document.querySelectorAll("textarea[data-slider]");
+	for (let i = 0; i < textareas.length; i++) {
+		const textarea = textareas[i],
+			code = {};
+
+		Object.assign(code, carouselConfigs[textarea.dataset.slider]);
+		code['container'] = undefined;
+		if (code !== undefined) {
+			const text = JSON.stringify(code, null, "  ");
+			textarea.innerHTML = text.replace(/"([^"]+)":/g, '$1:');
+		}
+	}
+
+	// initialize carousels
+	for (const [key, value] of Object.entries(carouselConfigs)) {
+		if (key === "events" || key === "autoplay")
+			continue;
+		ddcarousel(value);
+	}
+	const events = ddcarousel(carouselConfigs['events']),
+		autoplay = ddcarousel(carouselConfigs['autoplay']);
+
+	function resizeContainer() {
+		document.querySelector(".ddcarousel.events").style.width = Math.floor(Math.random() * 80) + 20 + "%";
+	}
+
+	document.getElementById("apStop").addEventListener("click", () => autoplay.module("autoplay").stop());
+	document.getElementById("apStart").addEventListener("click", () => autoplay.module("autoplay").start());
+	document.getElementById("resizeContainer").addEventListener("click", () => resizeContainer());
+
+	document.getElementById("eventsApStart").addEventListener("click", () => events.module("autoplay").start());
+	document.getElementById("eventsApStop").addEventListener("click", () => events.module("autoplay").stop());
+	document.getElementById("eventsGoToSlide").addEventListener("click", () => events.changePage(parseInt(document.getElementById("inputGoToSlide").value)));
+	document.getElementById("eventsPrevSlide").addEventListener("click", () => events.prevPage());
+	document.getElementById("eventsNextSlide").addEventListener("click", () => events.nextPage());
+	document.getElementById("eventsGetCurrentPage").addEventListener("click", () => writeLog(events.getCurrentPage()));
+	document.getElementById("eventsGetTotalPages").addEventListener("click", () => writeLog(events.getTotalPages()));
+	document.getElementById("eventsGetTotalSlides").addEventListener("click", () => writeLog(events.getTotalSlides()));
+	document.getElementById("eventsDestroyKeep").addEventListener("click", () => events.destroy());
+	document.getElementById("eventsDestroy").addEventListener("click", () => events.destroy(true));
+	document.getElementById("eventsInit").addEventListener("click", () => events.init(carouselConfigs['events']));
+	document.getElementById("eventsAddSlide").addEventListener("click", () => addSlide());
+	document.getElementById("eventsGetStatus").addEventListener("click", () => getStatus());
+});
