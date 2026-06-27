72cc1e9d981bbf379cae9ad870a8b79b57b4ebcb
Danail Dinev <9660431+danaildinev@users.noreply.github.com>
2026-06-27 13:13:39 +0300

    fix: update ddcarousel entry function to accept optional config

--- src/ddcarousel.ts
+++ src/ddcarousel.ts
@@ -2,6 +2,6 @@ import "./ddcarousel.scss";
 import Carousel from "./core/carousel";
 import type { CarouselConfig } from "./types/carousel.types";
 
-export default function ddcarousel(config: CarouselConfig) {
+export default function ddcarousel(config?: Partial<CarouselConfig>) {
     return new Carousel(config);
 }
